599314598c155b6994180ad7a4717716910ac86c
John Sasser <cw.john.sasser@gmail.com>
2020-06-24 13:31:04 -0400

    reformatted to stateful  component format

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,61 +1,86 @@
 import React from "react";
+import "./style.css"
 
-const ContactForm = (props) => {
-  return (
-    <div>
-      <section className="section section-dark"></section>
-      <div className="container contactContainer">
-        <div className="row">
-          <div className="col-sm-12 col-md-12 col-lg-12">
-            <div className="card-body cardContainer" id="email-container">
-              <h2 className="card-title">SHOOT ME AN EMAIL</h2>
+export default class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitForm = this.submitForm.bind(this);
+    this.state = {
+      status: "",
+    };
+  }
 
-              <hr size="3" color="#000" width="100%" />
-
-              {/* ***** need to add the formspree url path.  Signing up with the AMC gmail account */}
-
-              <form action="https://formspree.io/mgenlwbr" method="POST">
-                <div className="form-group">
-                  <label htmlFor="name">Name</label>
-                  <input
-                    type="text"
-                    name="name"
-                    className="form-control"
-                    placeholder="Your Name"
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="replyto">Your Email</label>
-                  <input
-                    type="email"
-                    name="replyto"
-                    className="form-control"
-                    placeholder="Your email@example.com"
-                  />
-                </div>
-                <div className="form-group">
-                  <label htmlFor="message">Message</label>
-                  <textarea
-                    className="form-control"
-                    name="message"
-                    rows="12"
-                    placeholder="Message"
-                  ></textarea>
-                  <br />
-
-                  <input
-                    className="btn btn-dark btn-lg btn-block submitButton"
-                    type="submit"
-                    value="Send"
-                  />
-                </div>
-              </form>
-            </div>
+  render() {
+    const { status } = this.state;
+    return (
+      <div className="email-form container">
+        <h3>SHOOT ME AN EMAIL</h3>
+        <form
+          onSubmit={this.submitForm}
+          action="https://formspree.io/mgenlwbr"
+          method="POST"
+        >
+          <div className="form-group">
+            <label htmlFor="name"></label>
+            <input
+              type="text"
+              name="name"
+              className="form-control"
+              placeholder="Your Name"
+            />
           </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+          <div className="form-group">
+            <label htmlFor="replyto"></label>
+            <input
+              type="email"
+              name="replyto"
+              className="form-control"
+              placeholder="Your: email@example.com"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="message"></label>
+            <textarea
+              className="form-control"
+              name="message"
+              rows="12"
+              placeholder="Message"
+            ></textarea>
+            <br />
 
-export default ContactForm;
+            {status === "SUCCESS" ? (
+              <p>Thanks!</p>
+            ) : (
+              <input
+                className="btn btn-dark btn-lg btn-block submitButton"
+                type="submit"
+                value="Send"
+              />
+            )}
+            {status === "ERROR" && <p>Ooops! There was an error, try again.</p>}
+            
+          </div>
+        </form>
+      </div>
+    );
+  }
+
+  submitForm(ev) {
+    ev.preventDefault();
+    const form = ev.target;
+    const data = new FormData(form);
+    const xhr = new XMLHttpRequest();
+    xhr.open(form.method, form.action);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState !== XMLHttpRequest.DONE) return;
+      if (xhr.status === 200) {
+        form.reset();
+        this.setState({ status: "SUCCESS" });
+      } else {
+        this.setState({ status: "ERROR" });
+      }
+    };
+    xhr.send(data);
+  }
+}
