ddcd372e95fa325a6264ea808b4c5e2a63fefa44
John Sasser <cw.john.sasser@gmail.com>
2020-06-24 01:22:46 -0400

    Home page and carousel component (template)

---
+++ client/src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Carousel from "../../components/Carousel"
+
+const HomePage = (props) => {
+  return (
+    <div>
+      <Carousel />
+    </div>
+  );
+};
+
+export default HomePage;
