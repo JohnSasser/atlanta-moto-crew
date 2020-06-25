6becf3a3bc0031e24b48e31e8e38eb90eff22bef
John Sasser <cw.john.sasser@gmail.com>
2020-06-25 17:13:11 -0400

    removing the modules, idk how they get in there

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -7,7 +7,6 @@ console.log('in the api/gallery GET ROUTE:')
   let keySecret = "kt9_w2gROmkumJEN6bBtk9no82c";
   let cloudinaryUrl = `https://884465835774119:${keySecret}@api.cloudinary.com/v1_1/di0f6kaus/resources/image`;
   
-  https://884465835774119:${keySecret}@api.cloudinary.com/v1_1/di0f6kaus
   axios
     .get(cloudinaryUrl)
     .then((res) => {
