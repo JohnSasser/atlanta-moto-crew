f0cd242d06ce9d22863eb699ca369de8cf27702e
John Sasser <cw.john.sasser@gmail.com>
2020-07-02 00:11:19 -0400

    working to dynamicly add publicId attributes to current gallery and carousel modal

--- client/src/pages/Gallery/index.js
+++ client/src/pages/Gallery/index.js
@@ -11,6 +11,7 @@ import axios from "axios";
 import { photos } from "./photos";
 
 const PhotoGallery = (props) => {
+  const [imagePublicIDs, setImagePublicIDs] = useState([{}]);
   const [currentImage, setCurrentImage] = useState(0);
   const [viewerIsOpen, setViewerIsOpen] = useState(false);
 
@@ -25,41 +26,62 @@ const PhotoGallery = (props) => {
   };
 
   useEffect(() => {
-    console.log("inside useEffect() - gallery.js");
-    // const response = axios.get("/api/gallery/images");
-    // console.log(response)
+    // console.log("inside useEffect() - gallery.js");
+    const CLOUDINARY_URL =
+      "https://res.cloudinary.com/di0f6kaus/image/list/2sTM.json";
 
-    axios.get('https://884465835774119:kt9_w2gROmkumJEN6bBtk9no82c@api.cloudinary.com/v1_1/di0f6kaus/resources/image/')
+    axios
+      .get(CLOUDINARY_URL)
       .then((res) => {
-        console.log("in the gallery fetch request");
-        console.log(res.body);
+        let imageList = res.data.resources;
+        // console.log("imageList:", imageList);
+
+        for (let i = 0; i < imageList.length; i++) {
+          let public_id = imageList[i].public_id;
+          // Setting the array of publicId's for CloudinaryContext Images;
+          setImagePublicIDs((imagePublicIDs) => [
+            {
+              ...imagePublicIDs,
+              public_id: public_id,
+              width: 8,
+              height: 6,
+              src: "",
+            },
+          ]);
+        }
       })
       .catch((err) => console.log(err));
   }, []);
 
+  console.log(imagePublicIDs);
+
   return (
     <div>
-      {/* <CloudinaryContext cloudName="di0f6kaus">
-        <div>
-          <Image publicId="" width="50" />
+      <CloudinaryContext cloudName="di0f6kaus">
+        {/* <div>
+          <Image publicId="amc-gallary/ronnie2_a0sogs" width="400" />
         </div>
-        <Image publicId="sample" width="0.5" />
-      </CloudinaryContext> */}
-      <Gallery photos={photos} onClick={openLightbox} />
-      <ModalGateway>
-        {viewerIsOpen ? (
-          <Modal onClose={closeLightbox}>
-            <Carousel
-              currentIndex={currentImage}
-              views={photos.map((x) => ({
-                ...x,
-                srcset: x.srcSet,
-                caption: x.title,
-              }))}
-            />
-          </Modal>
-        ) : null}
-      </ModalGateway>
+        <div>
+          <Image publicId="amc-gallary/ronnie_mfwl7i" width="400" />
+        </div>
+        <Image publicId="sample" width="0.5" /> */}
+
+        <Gallery photos={photos} onClick={openLightbox} />
+        <ModalGateway>
+          {viewerIsOpen ? (
+            <Modal onClose={closeLightbox}>
+              <Carousel
+                currentIndex={currentImage}
+                views={photos.map((x) => ({
+                  ...x,
+                  srcset: x.srcSet,
+                  caption: x.title,
+                }))}
+              />
+            </Modal>
+          ) : null}
+        </ModalGateway>
+      </CloudinaryContext>
     </div>
   );
 };
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-// const cors = require("cors");
+const cors = require("cors");
 const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
 // api routes
 const apiRoutes = require("./routes/apiRoutes");
@@ -23,45 +23,49 @@ if (process.env.NODE_ENV === "production") {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// app.use(cors());
+app.use(cors());
 // {
 //   origin: ["http://localhost:3000"],
 //   credentials: true,
 // }
+const BASE_URL = 'https://api.cloudinary.com/v1_1/di0f6kaus/resources/image'
+
 
 // CUSTOM PROXY ROUTE FOR HEADERS - CORS WAS NOT WORKING;
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', '*');
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
-// app.get('/api/gallery/images', (req, res) => {
-//   request(
-//     { url: 'https://api.cloudinary.com/v1_1/di0f6kaus/resources/image/tags/2sTM'},
-//     (error, response, body) => {
-//       if (error || response.status !== 200) {
-//         return res.status(500).json({
-//           type: error,
-//           message: error.message
-//         });
-//       }
-//       console.log(req);
-//       res.json(JSON.parse(body));
-//     }
-//   )
-// });
+app.get('/api/gallery/images', (req, res) => {
+  request(
+    { url: BASE_URL },
+    (error, response, body) => {
+      if (error || response.status !== 200) {
+        return res.status(500).json({
+          type: error,
+          message: error.message
+        });
+      }
+      console.log(req);
+      res.json(JSON.parse(body));
+    }
+  )
+});
 
+setImagePublicIDs(imagePublicIDs => [...imagePublicIDs, public_id])
 // API ROUTES FOR REQUESTS
-app.use(apiRoutes);
+// app.use(apiRoutes);
 
 // Send every request to the React app
 app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodDB://localhost/atlantamotocrewdb"
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodDB://localhost/atlantamotocrewdb",
+//   { useNewUrlParser: true }
+// );
 
 app.listen(PORT, function () {
   // console.log(`🌎 ==> API server now on port ${PORT}!`);
